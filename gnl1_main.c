ca02a3f2bdc59d93f5733a24dbf59fd7a5a2cc2f
andjajas <adjajasaputra@gmail.com>
2026-05-31 14:36:36 +0200

    gnl1_main.c gnl_xmpl_main.c

---
+++ gnl1_main.c
@@ -0,0 +1,23 @@
+#include "gnl_gemini.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("test.txt", O_RDONLY );
+	if (fd == -1)
+		return (1);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
---
+++ gnl_xmpl_main.c
@@ -0,0 +1,30 @@
+#include "gnl_gemini.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int		line_count;
+
+	// Open a text file in read-only mode
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error: Could not open file.\n");
+		return (1);
+	}
+	
+	line_count = 1;
+	// Loop until get_next_line returns NULL (End of File or Error)
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("Line [%d]: %s", line_count, line);
+		free(line); // CRITICAL: You must free the memory allocated by GNL
+		line_count++;
+	}
+
+	close(fd);
+	return (0);
+}
