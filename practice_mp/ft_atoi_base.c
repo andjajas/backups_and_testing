1ef9adf3827be2e45017d8249880092c13417ede
Andre Djajasaputra <andjajas@f1r6s9.codam.nl>
2026-06-21 11:39:53 +0200

    ft_atoi_base.c fixed

--- practice_mp/ft_atoi_base.c
+++ practice_mp/ft_atoi_base.c
@@ -1,8 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_atoi_base.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: andjajas <andjajas@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/06/21 11:27:37 by andjajas      #+#    #+#                 */
+/*   Updated: 2026/06/21 11:38:49 by andjajas      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	convert_base_c(char c, unsigned int base);
+
 int	ft_atoi_base(const char	*nbr, unsigned int base)
 {
 	unsigned int	i;
-	int		sign;
-	int		decimal;
+	int				sign;
+	int				decimal;
 
 	i = 0;
 	sign = 1;
@@ -23,5 +39,30 @@ int	ft_atoi_base(const char	*nbr, unsigned int base)
 	return (decimal * sign);
 }
 
-int	convert_base_c(char c, base)
-{}
+int	convert_base_c(char c, unsigned int base)
+{
+	unsigned int	i;
+	char			*lower_c_base;
+	char			*upper_c_base;
+
+	i = 0;
+	lower_c_base = "0123456789abcdef";
+	upper_c_base = "0123456789ABCDEF";
+	while (i < base)
+	{
+		if (c == lower_c_base[i] || c == upper_c_base[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_atoi_base("-1011", 2));
+	printf("%d\n", ft_atoi_base("1011", 2));
+	printf("%d\n", ft_atoi_base("1A", 16));
+	printf("%d\n", ft_atoi_base("7F", 16));
+	printf("%d\n", ft_atoi_base("11", 16));
+	return (0);
+}
