2b2e8f7fff83f1ab73b7031f486da94a3b919e81
Andre Djajasaputra <andjajas@f0r1s14.codam.nl>
2026-06-27 16:58:54 +0200

    clearer variable names ft_union.c

--- practice_mp/lvl01/ft_union.c
+++ practice_mp/lvl01/ft_union.c
@@ -6,13 +6,13 @@
 /*   By: andjajas <andjajas@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/06/21 17:00:12 by andjajas      #+#    #+#                 */
-/*   Updated: 2026/06/21 17:51:42 by andjajas      ########   odam.nl         */
+/*   Updated: 2026/06/27 16:56:24 by andjajas      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	check_char(char	*str, char c, int ndx)
+int	not_printed(char *str, char c, int ndx)
 {
 	int	i;
 
@@ -39,15 +39,15 @@ int	main(int argc, char **argv)
 	j = 0;
 	while (argv[1][j])
 	{
-		if (check_char(argv[1], argv[1][j], j) == 1)
+		if (not_printed(argv[1], argv[1][j], j) == 1)
 			write(1, &argv[1][j], 1);
 		j++;
 	}
 	k = 0;
 	while (argv[2][k])
 	{
-		if ((check_char(argv[1], argv[2][k], j) == 1)
-			&& (check_char(argv[2], argv[2][k], k) == 1))
+		if ((not_printed(argv[1], argv[2][k], j) == 1)
+			&& (not_printed(argv[2], argv[2][k], k) == 1))
 			write(1, &argv[2][k], 1);
 		k++;
 	}
